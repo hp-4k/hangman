c605772f3979fa8d5726172daaa1a8e2bb7b7edf
Hubert Pompecki <hpompecki@gmail.com>
2015-11-29 09:48:16 +0000

    initial commit

---
+++ lib/game.rb
@@ -0,0 +1,93 @@
+class Game
+  
+  DICTIONARY_FILE = "5desk.txt"
+  CHANCES = 10
+
+  def initialize
+    @secret_word = nil
+    @guessed_letters = []
+    @guesses = []
+    @lives_left = 0
+  end
+
+  def play
+    select_word
+    prepare_game
+    until game_over?
+      process_guess(obtain_guess)
+    end
+    end_game_message
+  end
+
+  private
+
+    def select_word
+      random_word = ''
+      until is_valid?(random_word)
+        random_word = dictionary_words.sample.strip
+        # puts "candiadte: #{random_word}"
+      end
+      @secret_word = random_word
+    end
+
+    def prepare_game
+      @guessed_letters = ['_'] * @secret_word.length
+      @lives_left = CHANCES
+    end
+
+    def is_valid?(word)
+      return false if /[A-Z]/ =~ word
+      word.length.between?(5,12)
+    end
+
+    def dictionary_words
+      File.readlines(DICTIONARY_FILE)
+    end
+
+    def game_over?
+      !lives_left? || word_guessed?
+    end
+
+    def lives_left?
+      @lives_left > 0
+    end
+
+    def word_guessed?
+      @guessed_letters.join('') == @secret_word
+    end
+
+    def obtain_guess
+      puts ''
+      puts "You have #{@lives_left} lives left."
+      puts "Guesses you have tried: #{@guesses.join(' ')}"
+      provide_feedback
+      puts "Please guess a character (only the first character will be read):"
+      gets.chomp[0]
+    end
+
+    def process_guess(character)
+      raise ArgumentError "more than 1 character supplied" if character.length != 1
+      @guesses << character
+      good_guess = false
+      @secret_word.split('').each_with_index do |secret_character, index|
+        if character.downcase == secret_character.downcase
+          @guessed_letters[index] = character.downcase
+          good_guess = true
+        end
+      end
+      @lives_left -= 1 unless good_guess
+    end
+
+    def provide_feedback
+      puts @guessed_letters.join(' ')
+    end
+
+    def end_game_message
+      if word_guessed?
+        puts "Congratulations! You have guessed the word - #{@secret_word}!"
+      else
+        puts "Sorry, you have run out of chances!"
+        puts "The secret word was '#{@secret_word}'"
+      end
+    end
+end
